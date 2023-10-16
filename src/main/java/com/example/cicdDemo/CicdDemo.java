0dcc2c1d9ecde0bb1b5114bb55de187d2e3f5718
Sadman Sakib <sadmansakib@Sadmans-MacBook-Air.local>
2023-10-16 11:40:39 +0600

    initial push
    Signed-off-by: Sadman Sakib <sadmansakib@Sadmans-MacBook-Air.local>

---
+++ src/main/java/com/example/cicdDemo/CicdDemo.java
@@ -0,0 +1,13 @@
+package com.example.cicdDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdDemo {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdDemo.class, args);
+	}
+
+}
